be43327574074a9d9042e478719293cb1a4e5076
pardeep-pathania <pathanianikhil145@gmail.com>
2025-01-26 12:30:57 +0530

    featureare added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
     <div>
      <nav className='flex justify-between bg-slate-700 text-white p-2'>
         <div className="logo">
-            <span class>iTask</span>
+            <span>iTask</span>
         </div>
     <ul className='flex gap-8 mx-8'>
-        <li className="cursor-pointer hover:font-bold transition-all">Home</li>
+        <li className="cursor-pointer hover:font-bold transition-all ">Home</li>
         <li className="cursor-pointer hover:font-bold transition-all">Your Tasks</li>
     </ul>
      </nav>
